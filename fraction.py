fb1dd217ac900023ac22004d1397458c3aac6dc1
Dimitri <115943634+Dimtiri19@users.noreply.github.com>
2023-12-12 12:48:45 +0100

    Add files via upload

---
+++ fraction.py
@@ -0,0 +1,170 @@
+import math
+from math import gcd
+class Fraction:
+    """Class representing a fraction and operations on it
+
+    Author : V. Van den Schrieck
+    Date : October 2021
+    This class allows fraction manipulations through several operations.
+    """
+
+    def __init__(self,num=0, den=1):
+        """Initialize a Fraction object.
+
+        PRE: den != 0
+        POST: Fraction is initialized with the given numerator and denominator,
+              reduced to its simplest form.
+        """
+        common = math.gcd(num, den)
+        self.num = num // common
+        self.den = den // common
+
+    @property
+    def numerator(self):
+        """Get the numerator of the fraction.
+
+        POST: Returns the numerator of the fraction.
+        """
+        return self.num
+    
+    @property
+    def denominator(self):
+        """Get the denominator of the fraction.
+
+        POST: Returns the denominator of the fraction.
+        """
+        return self.den
+
+# ------------------ Textual representations ------------------
+
+    def __str__(self) :
+        """Return a textual representation of the reduced form of the fraction.
+
+        POST: Returns a string representation of the fraction.
+        """
+        return f"{self.num}/{self.den}"
+
+    def as_mixed_number(self) :
+        """Return a textual representation of the reduced form of the fraction as a mixed number.
+
+        POST: Returns a string representation of the fraction as a mixed number.
+        """
+        whole_part = self.num // self.den
+        remainder = self.num % self.den
+        return f"{whole_part} {remainder}/{self.den}"
+
+    
+# ------------------ Operators overloading ------------------
+
+    def __add__(self, other):
+        """Overloading of the + operator for fractions.
+        
+        PRE: other.den != 0
+        POST: Returns a new Fraction object representing the sum of self and other.
+        """
+        new_num = self.num * other.den + other.num * self.den
+        new_den = self.den * other.den
+        return Fraction(new_num, new_den)
+
+
+    def __sub__(self, other):
+        """Overloading of the - operator for fractions.
+
+        PRE: other.den != 0
+        POST: Returns a new Fraction object representing the difference of self and other.
+        """
+        new_num = self.num * other.den - other.num * self.den
+        new_den = self.den * other.den
+        return Fraction(new_num, new_den)
+
+
+    def __mul__(self, other):
+        """Overloading of the * operator for fractions.
+
+        PRE: other.den != 0
+        POST: Returns a new Fraction object representing the product of self and other.
+        """
+        new_num = self.num * other.num
+        new_den = self.den * other.den
+        return Fraction(new_num, new_den)
+
+
+    def __truediv__(self, other):
+        """Overloading of the / operator for fractions.
+
+        PRE: other.den != 0 and other.num != 0
+        POST: Returns a new Fraction object representing the division of self by other.
+        """
+        new_num = self.num * other.den
+        new_den = self.den * other.num
+        return Fraction(new_num, new_den)
+
+
+    def __pow__(self, other):
+        """Overloading of the ** operator for fractions.
+
+        PRE: self.den != 0
+        POST: Returns a new Fraction object representing self raised to the power of the given exponent.
+        """
+        new_num = self.num ** (other.num / other.den)
+        new_den = self.den ** (other.num / other.den)
+
+        return Fraction(new_num, new_den)
+    
+    
+    def __eq__(self, other) : 
+        """Overloading of the == operator for fractions.
+
+        POST: Returns True if self is equal to other, False otherwise.
+        """
+        return self.num * other.den == other.num * self.den
+        
+    def __float__(self) :
+        """Returns the decimal value of the fraction.
+
+        POST: Returns the decimal value of the fraction as a float.
+        """
+        return self.num / self.den
+    
+# TODO : [BONUS] You can overload other operators if you wish (ex : <, >, ...)
+
+
+
+# ------------------ Properties checking  ------------------
+
+    def is_zero(self):
+        """Check if a fraction's value is 0.
+
+        POST: Returns True if the fraction is zero, False otherwise.
+        """
+        return self.num == 0
+
+
+    def is_integer(self):
+        """Check if a fraction is an integer.
+
+        POST: Returns True if the fraction is an integer, False otherwise.
+        """
+        return self.den == 1
+    
+    def is_proper(self):
+        """Check if the absolute value of the fraction is less than 1.
+
+        POST: Returns True if the fraction is a proper fraction, False otherwise.
+        """
+        return abs(self.num) < abs(self.den)
+
+    def is_unit(self):
+        """Check if a fraction's numerator is 1 in its reduced form.
+
+        POST: Returns True if the fraction is a unit fraction, False otherwise.
+        """
+        return self.num == 1
+
+    def is_adjacent_to(self, other) :
+        """Check if two fractions differ by a unit fraction.
+
+        POST: Returns True if the absolute value of the difference between self and other is a unit fraction, False otherwise.
+        """
+        return abs(self.num * other.den - other.num * self.den) == 1
+
---
+++ tp7.py
@@ -0,0 +1,35 @@
+from fraction import Fraction
+
+# Création de fractions
+fraction1 = Fraction(1, 2)
+fraction2 = Fraction(3, 4)
+fraction3 = Fraction(2, 3)
+print(fraction1, fraction2, fraction3)
+# Opérations avec les fractions
+sum_fraction = fraction1 + fraction2
+diff_fraction = fraction1 - fraction3
+prod_fraction = fraction2 * fraction3
+div_fraction = fraction1 / fraction2
+pow_fraction = fraction3 ** fraction1
+
+# Affichage des résultats
+print(f"Sum: {sum_fraction}")
+print(f"Difference: {diff_fraction}")
+print(f"Product: {prod_fraction}")
+print(f"Division: {div_fraction}")
+#print(f"Power: {pow_fraction}")
+
+# Utilisation des méthodes supplémentaires
+print(f"Is zero? {fraction1.is_zero()}")
+print(f"Is an integer? {fraction2.is_integer()}")
+print(f"Is proper fraction? {fraction3.is_proper()}")
+print(f"Is a unit fraction? {fraction1.is_unit()}")
+print(f"Is adjacent to fraction3? {fraction1.is_adjacent_to(fraction3)}")
+
+# Utilisation de la méthode as_mixed_number
+mixed_number = fraction2.as_mixed_number()
+print(f"Fraction 2 as a mixed number: {mixed_number}")
+
+# Conversion en float
+float_value = float(fraction3)
+print(f"Float value of fraction3: {float_value}")
